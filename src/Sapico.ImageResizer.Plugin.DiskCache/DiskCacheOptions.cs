fa3341fac9b4708b41ffc41d8876e1ddaa352c84
Nico Sap <NicoJuicy@users.noreply.github.com>
2026-03-18 01:25:41 +0100

    Minor cleanup

--- src/Sapico.ImageResizer.Plugin.DiskCache/DiskCacheOptions.cs
+++ src/Sapico.ImageResizer.Plugin.DiskCache/DiskCacheOptions.cs
@@ -1,11 +1,9 @@
-using System;
 using System.IO;
 
-namespace Sapico.ImageResizer.Plugin.DiskCache
+namespace Sapico.ImageResizer.Plugin.DiskCache;
+
+public class DiskCacheOptions
 {
-    public class DiskCacheOptions
-    {
-        public string CacheFolder { get; set; } = Path.Combine(Path.GetTempPath(), "img-cache"); // Default folder for docker containers
-            //Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Te), "img-cache");
-    }
+    public string CacheFolder { get; set; } = Path.Combine(Path.GetTempPath(), "img-cache"); // Default folder for docker containers
+        //Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Te), "img-cache");
 }
